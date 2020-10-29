7f3a49f1a21c290c4796765c03cdfa456a7074b8
octahate <geli961212@hotmail.com>
2020-10-29 14:02:45 +0100

    command line registration form, to test my skills

---
+++ src/main/java/classtructureio/Registration.java
@@ -0,0 +1,21 @@
+package classtructureio;
+
+import java.util.Scanner;
+
+public class Registration {
+    public static void main(String[] args) {
+        String nameQ = "Please tell me your name:";
+        String mailQ = "Please tell me your e-mail address:";
+        String credentials = "Your user data:";
+
+        System.out.println(nameQ);
+        Scanner infos = new Scanner(System.in);
+        String name = infos.nextLine();
+        System.out.println(mailQ);
+        String email = infos.nextLine();
+        System.out.println(credentials);
+        System.out.println(name);
+        System.out.println(email);
+
+    }
+}
