d33a882301f0fda38bacee061cd7503315c315a9
octahate <geli961212@hotmail.com>
2020-10-29 15:53:53 +0100

    updqated with methods

--- src/main/java/classstructureattributes/Trainer.java
+++ src/main/java/classstructureattributes/Trainer.java
@@ -3,4 +3,13 @@ package classstructureattributes;
 public class Trainer {
     String name;
     int yearOfBirth;
+    public String getName() {
+        return name;
+    }
+    public String getNameAndYearOfBirth () {
+        return name + " " + yearOfBirth;
+    }
+    public void changeName(String newName){
+    name = newName;
+    }
 }
--- src/main/java/classstructureattributes/TrainerMain.java
+++ src/main/java/classstructureattributes/TrainerMain.java
@@ -1,3 +1,19 @@
 package classstructureattributes;
 public class TrainerMain {
+    public static void main(String[] args) {
+        Trainer trainer = new Trainer(); // 1
+        trainer.name = "John Doe";
+        trainer.yearOfBirth = 1980;
+        System.out.println(trainer.name);
+        System.out.println(trainer.yearOfBirth);
+        Trainer anotherTrainer = new Trainer(); // 2
+        anotherTrainer.name = "Jack Doe";
+        System.out.println(anotherTrainer.getName());
+        System.out.println(trainer.getNameAndYearOfBirth());
+        anotherTrainer.changeName("Joe Doe");
+        System.out.println(anotherTrainer.getName());
+
+    }
+
 }
+
