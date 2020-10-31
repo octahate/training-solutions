8dc3fdae9b6341f0aa61a1303fb590e951487ade
octahate <geli961212@hotmail.com>
2020-10-31 12:32:04 +0100

    typo edits

--- src/main/java/classstructureconstructors/Store.java
+++ src/main/java/classstructureconstructors/Store.java
@@ -1,24 +1,24 @@
 package classstructureconstructors;
 
 public class Store {
+
     private int stock;
     private String product;
+
     public int getStock() {
         return stock;
+    }
+
+    public Store (String product) {
+        this.product = product;
+        stock = 0;
     }
 
     public String getProduct() {
         return product;
     }
 
-
-
-
-   public Store (String product) {
-       this.product = product;
-       stock = 0;
-   }
-   public void store (int amount) {
+      public void store (int amount) {
        this.stock = stock + amount;
    }
     public void dispatch (int amount) {
