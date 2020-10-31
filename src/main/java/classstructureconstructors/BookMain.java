5b6534d7dfcf1a800daed8c4445d7b585ea2d740
octahate <geli961212@hotmail.com>
2020-10-31 12:28:30 +0100

    cosmetics

--- src/main/java/classstructureconstructors/BookMain.java
+++ src/main/java/classstructureconstructors/BookMain.java
@@ -3,10 +3,10 @@ package classstructureconstructors;
 public class BookMain {
     public static void main(String[] args) {
         Book book = new Book("Orwell", "1984");
-        System.out.println(book.getAuthor());
-        System.out.println(book.getTitle());
+        System.out.println("Szerző: " + book.getAuthor());
+        System.out.println("Cím: " + book.getTitle());
         book.setRegNumber("991");
-        System.out.println(book.getRegNumber());
+        System.out.println("Nyilvántartási szám: " + book.getRegNumber());
     }
 
 }
