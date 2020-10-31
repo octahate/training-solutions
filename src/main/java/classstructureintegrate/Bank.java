6d25f7423e779c38fda176546dafb0ba341f5b08
octahate <geli961212@hotmail.com>
2020-10-31 14:19:58 +0100

    bank finally works

--- src/main/java/classstructureintegrate/Bank.java
+++ src/main/java/classstructureintegrate/Bank.java
@@ -3,16 +3,21 @@ package classstructureintegrate;
 public class Bank {
     public static void main(String[] args) {
         BankAccount accountone = new BankAccount("1123534", "Geli", 120);
+        System.out.println(accountone.getInfo());
         accountone.deposit(12);
         System.out.println(accountone.getInfo());
         accountone.withdraw(4);
         System.out.println(accountone.getInfo());
         BankAccount accounttwo = new BankAccount("11223434","Peti", 56102);
+        System.out.println(accountone.getInfo());
         accounttwo.deposit(12);
         System.out.println(accounttwo.getInfo());
         accounttwo.withdraw(4);
         System.out.println(accounttwo.getInfo());
-        accounttwo.transfer(ac, 365);
+        accountone.transfer(accounttwo, 365);
+        System.out.println(accounttwo.getInfo());
+        System.out.println(accountone.getInfo());
+        accounttwo.transfer(accountone, 365);
         System.out.println(accounttwo.getInfo());
         System.out.println(accountone.getInfo());
 
