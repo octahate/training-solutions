c439a92134a4dbdd8d3f62fc2bac282ce3c75223
octahate <geli961212@hotmail.com>
2020-11-01 12:05:59 +0100

    api done

---
+++ src/main/java/javaapi/Upper.java
@@ -0,0 +1,9 @@
+package javaapi;
+
+public class Upper {
+
+    public static void main(String[] args) {
+        String Hw = "Hello World!";
+        System.out.println(Hw.toUpperCase());
+    }
+}
