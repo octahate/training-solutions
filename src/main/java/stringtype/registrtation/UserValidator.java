ae6cb96b25505666b41a5e4883a940d537240725
octahate <geli961212@hotmail.com>
2020-11-01 13:37:47 +0100

    I got confused by the cases, will return to this later

---
+++ src/main/java/stringtype/registrtation/UserValidator.java
@@ -0,0 +1,17 @@
+package stringtype.registrtation;
+
+public class UserValidator {
+    public class UserValidator {
+        public boolean isValidUsername(String username){
+            return username.length() > 0;
+        }
+        public boolean isValidPassword (String password1, String password2){
+            return password1.equals(password2)
+                    && password1.length() >= 8;
+        }
+        public boolean isValidEmail (String email){
+            return (
+
+        }
+    }
+}
