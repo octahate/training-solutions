abd3cc8d07ef25c650bca0d5401acdc98b80df98
octahate <geli961212@hotmail.com>
2020-11-02 09:54:32 +0100

    introcontrol start

---
+++ src/main/java/introcontrol/IntroControl.java
@@ -0,0 +1,12 @@
+package introcontrol;
+
+public class IntroControl {
+
+    public int substractTenIfGreaterThanTen (int number) {
+      if (number <= 10) {
+          return number;
+      } else {
+          return (number - 10);
+        }
+    }
+}
---
+++ src/main/java/introcontrol/IntroControlMain.java
@@ -0,0 +1,11 @@
+package introcontrol;
+
+public class IntroControlMain {
+    public static void main(String[] args) {
+        IntroControl introcontrol = new IntroControl();
+        System.out.println(introcontrol.substractTenIfGreaterThanTen(15));
+        System.out.println(introcontrol.substractTenIfGreaterThanTen(10));
+        System.out.println(introcontrol.substractTenIfGreaterThanTen(32));
+
+    }
+}
