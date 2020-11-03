e3446cb859efcfbdf4c2123ed79545f9e7e92679
octahate <geli961212@hotmail.com>
2020-11-03 10:23:46 +0100

    extra done

--- src/main/java/week02/Phone.java
+++ src/main/java/week02/Phone.java
@@ -30,14 +30,18 @@ public class Phone {
     public static void main(String[] args) {
         Phone phone1 = new Phone("Alcatel", 25);
         Phone phone2 = new Phone("Nokia", 256);
+
         System.out.println(phone1.getType());
         System.out.println(phone1.getMem() + " MB");
         System.out.println(phone2.getType());
+
         System.out.println(phone1.getMem() + " MB");
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Telefon típusa:");
         String phone3type = scanner.nextLine();
         System.out.println("Telefon memóriája:");
+
         int phone3mem = scanner.nextInt();
         Phone phone3 = new Phone(phone3type, phone3mem);
         System.out.println(phone3.getType());
