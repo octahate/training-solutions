bf0ccea54c76fe0ea521458e98e6d7ed62a9bafb
octahate <geli961212@hotmail.com>
2020-11-03 10:16:18 +0100

    first part done

---
+++ src/main/java/week02/Phone.java
@@ -0,0 +1,39 @@
+package week02;
+
+public class Phone {
+    private String type;
+    private int mem;
+
+    public Phone(String type, int mem) {
+        this.type = type;
+        this.mem = mem;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getMem() {
+        return mem;
+    }
+
+    public void setMem(int mem) {
+        this.mem = mem;
+    }
+
+    public static void main(String[] args) {
+        Phone phone1 = new Phone("Alcatel", 25);
+        Phone phone2 = new Phone("Nokia", 256);
+        System.out.println(phone1.getType());
+        System.out.println(phone1.getMem());
+        System.out.println(phone2.getType());
+        System.out.println(phone1.getMem());
+        S
+
+
+    }
+}
