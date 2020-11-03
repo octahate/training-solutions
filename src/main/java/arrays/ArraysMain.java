09b66e761d89f827be0a3ac556c1f19b60d276af
octahate <geli961212@hotmail.com>
2020-11-03 23:37:21 +0100

    done without the extra... I guess

---
+++ src/main/java/arrays/ArraysMain.java
@@ -0,0 +1,33 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArraysMain {
+    public String numberOfDaysAsString() {
+        int[] numberOfDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        return numberOfDays.toString();
+    }
+    public List<String> daysOfWeek() {
+        return Arrays.asList("Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat", "Vasárnap");
+    }
+    public boolean sameTempValues(double[] day, double[] anotherDay){
+        return Arrays.equals(day,anotherDay);
+    }
+    public boolean wonLottery (int[] a, int[] b) {
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return Arrays.equals(a,b);
+    }
+
+    public static void main(String[] args) {
+        ArraysMain arraysMain = new ArraysMain();
+        int[] a = {1,2,3,4};
+        int[] b = {4,3,2,1};
+        System.out.println(arraysMain.wonLottery (a,b));
+
+    }
+
+
+
+}
