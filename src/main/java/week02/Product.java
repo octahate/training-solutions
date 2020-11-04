ec33e4b6101da1a75ccffb396e690a84b95c9431
octahate <geli961212@hotmail.com>
2020-11-04 15:55:26 +0100

    done

--- src/main/java/week02/Product.java
+++ src/main/java/week02/Product.java
@@ -20,20 +20,24 @@ public class Product {
         return name.equals(p.getName()); // elfelejtetted a getterét a pnek
     }
     public boolean areTheyEqualPlus (Product p){
-        return name.equals(p) && code.length()
+        return name.equals(p) && (code.length() + 1) >= p.getCode().length();
+
     }
 
 
         public static void main(String[] args) {
         Product product1 = new  Product("Alma","001");
 
-        Product product2 = new  Product("Alma","001");
+        Product product2 = new  Product("Alma","0001");
 
         Product product3 = new  Product("Körte","002");
 
         System.out.println(product2.areTheyEqual(product1));
         System.out.println(product3.areTheyEqual(product1));
         System.out.println(product1.areTheyEqual(product2));
+        System.out.println(product2.areTheyEqualPlus(product1));
+        System.out.println(product3.areTheyEqualPlus(product2));
+        System.out.println(product1.areTheyEqualPlus(product3));
 
 
 
