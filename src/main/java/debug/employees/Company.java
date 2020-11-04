ce7e3e4dd31d58cbdde31b489760d37df31b9b8e
octahate <geli961212@hotmail.com>
2020-11-04 21:06:26 +0100

    employees half done

--- src/main/java/debug/employees/Company.java
+++ src/main/java/debug/employees/Company.java
@@ -2,14 +2,33 @@ package debug.employees;
 
 import debug.employees.Employee;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Company {
 
-    private List<Employee> employee;
+    private List<Employee> employees;
 
-    public Company (List<Employee> workers){
-        employee = workers;
+    public Company (List<Employee> employees){
+        this.employees = employees;
+    }
+    public addEmployee (Employee employee){
+        employees.add(employee);
     }
 
+    public Employee findEmployeeByName (String name) {
+        for (Employee worker: employees) {
+            if (worker.getName().equals(name)) {
+                return worker;
+            }
+        }
+        return null;
+    }
+    public List <String> listEmployeeNames() {
+        List <String> names = new ArrayList<>();
+        for (Employee employee: employees) {
+            names.add(employee.getName());
+        }
+        return names;
+    }
 }
--- src/main/java/debug/numbers/NumberStatistics.java
+++ src/main/java/debug/numbers/NumberStatistics.java
@@ -1,4 +1,35 @@
 package debug.numbers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NumberStatistics {
+    private List<Integer> numbers;
+
+    public NumberStatistics(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public static void main(String[] args) {
+        public int sumPositives() {
+            int sum = 0;
+            for(int n: numbers) {
+                if(n != 0) {
+                    sum += n;
+                }
+            }
+            return sum;
+        }
+
+        public int minDifferenceBetweenNeighbours() {
+            int minDifference = numbers.get(0) - numbers.get(1) >= 0 ? numbers.get(0) - numbers.get(1) : numbers.get(1) - numbers.get(0);
+            for(int i = 1; i < numbers.size() - 1; i++) {
+                int actDifference = numbers.get(i) - numbers.get(i + 1);
+                if(actDifference < minDifference) {
+                    minDifference = actDifference;
+                }
+            }
+            return minDifference;
+        }
+    }
 }
