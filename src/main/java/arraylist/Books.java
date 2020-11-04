2a3a1cabaefa980412f24c8b38d01826d7e6fd90
octahate <geli961212@hotmail.com>
2020-11-04 18:00:20 +0100

    books done, although the prefix is the first 6 letters

---
+++ src/main/java/arraylist/Books.java
@@ -0,0 +1,38 @@
+package arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Books {
+    private List<String> books = new ArrayList<>();
+
+    public void add(String book) {
+        books.add(book);
+    }
+
+    public List<String> findAllByPrefix(String prefix) {
+        List<String> results = new ArrayList<>();
+        for (String title : books) {
+            if (prefix.equals(title.substring(0, 5))) {
+                results.add(title);
+            }
+        }
+        return results;
+    }
+
+    public List<String> getTitles(){
+        return books;
+    }
+
+    public static void main(String[] args) {
+        Books books = new Books();
+        books.add("A Kis Herceg");
+        books.add("Tenkes Kapitánya");
+        books.add("Kincskereső Kisködmön");
+        books.add("Harry Potter és a Tűz Serlege");
+        books.add("Harry Potter és a Bölcsek Köve");
+        System.out.println(books.getTitles());
+        System.out.println(books.findAllByPrefix("Harry"));
+
+    }
+}
