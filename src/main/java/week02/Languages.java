2a4ce0e6662c5fea76835b74721ce38c24a3e196
octahate <geli961212@hotmail.com>
2020-11-06 15:54:25 +0100

    now it works

---
+++ src/main/java/week02/Languages.java
@@ -0,0 +1,17 @@
+package week02;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Languages {
+    public static void main(String[] args) {
+        List<String> List = Arrays.asList("Java","Python","JavaScript");
+        for (int i= 0; i < List.size(); i++){
+            if (List.get(i).length() > 5) {
+                System.out.println(List.get(i));
+            }
+        }
+
+    }
+}
