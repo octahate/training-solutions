69e8277534e5a7b148054ddf5b2322a51da76d20
octahate <geli961212@hotmail.com>
2020-11-16 23:33:01 +0100

    edits

--- src/main/java/week04/NameChanger.java
+++ src/main/java/week04/NameChanger.java
@@ -1,6 +1,24 @@
 package week04;
 
 public class NameChanger {
-    private String fullName;
+    private String fullname;
     //sajnos még nem jutottam el a StringBuilderig és a kivételkezelésig :(
+
+
+        public NameChanger(String fullname) {
+            if (fullname == null || fullname.isBlank()) {
+                throw new IllegalArgumentException("Invalid name: " + fullname);
+            }
+            this.fullname = fullname.trim();
+        }
+
+        public String getFullname() {
+            return fullname;
+        }
+
+        public void changeFirsName(String firstName) {
+            String str = firstName.trim();
+            fullname = fullname.replace(fullname.substring(fullname.indexOf(" ") + 1), str);
+        }
+    }
 }
