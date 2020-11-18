9841bbacd694453a7c2a1196a48ed8bb0e0dc616
octahate <geli961212@hotmail.com>
2020-11-18 16:04:02 +0100

    done

---
+++ src/main/java/week04/NumberGuesser.java
@@ -0,0 +1,29 @@
+package week04;
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class NumberGuesser {
+  public static void main(String[] args) {
+        int random = new Random().nextInt (100) + 1;
+        Scanner scanner = new Scanner(System.in);
+
+        boolean ok = false;
+
+        while (!ok){
+            System.out.println("Kérek egy számot!");
+            int guess = Integer.parseInt(scanner.nextLine());
+            if (guess > random) {
+                System.out.println("Kisebb!"); } else
+                    if (guess < random) {
+                    System.out.println("Nagyobb!");} else {
+                    System.out.println("yupp!");
+                    ok=true;
+                }
+            }
+        }
+    }
+
+
