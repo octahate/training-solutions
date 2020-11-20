c53e511eaee667a420dd7745a75921439eff5d0d
octahate <geli961212@hotmail.com>
2020-11-20 15:55:29 +0100

    done

---
+++ src/main/java/week04/Search.java
@@ -0,0 +1,17 @@
+package week04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Search {
+        public List<Integer> getIndexesOfChar(String str, char c) {
+            List<Integer> result = new ArrayList<>();
+            for (int i=0; i< str.length(); i++)
+                str.charAt(i) == char temp;
+            if (char.equals(temp)) {
+                result.add(str.indexOf(c));
+            }
+                return result;
+        }
+    }
+}
---
+++ src/main/java/week04/SearchMain.java
@@ -0,0 +1,6 @@
+package week04;
+
+public class SearchMain {
+    Search search = new Search();
+
+}
