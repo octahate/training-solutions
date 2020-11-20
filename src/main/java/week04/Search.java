c94f2d778614cd92cef470a422978af669924d63
octahate <geli961212@hotmail.com>
2020-11-20 15:59:09 +0100

    done

--- src/main/java/week04/Search.java
+++ src/main/java/week04/Search.java
@@ -7,7 +7,7 @@ public class Search {
         public List<Integer> getIndexesOfChar(String str, char c) {
             List<Integer> result = new ArrayList<>();
             for (int i=0; i< str.length(); i++)
-                str.charAt(i) == char temp;
+                str.charAt(i) = temp;
             if (c == temp) {
                 result.add(str.indexOf(c));
             }
