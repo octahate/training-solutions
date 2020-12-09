6348942792ef85b689c030f90be7320de9b83f90
octahate <geli961212@hotmail.com>
2020-12-09 20:08:16 +0100

    stats done

---
+++ src/main/java/methodvarargs/ExamStats.java
@@ -0,0 +1,30 @@
+package methodvarargs;
+
+public class ExamStats {
+    private int maxPoints;
+
+    public ExamStats(int maxPoints) {
+        this.maxPoints = maxPoints;
+    }
+
+    public int getNumberOfTopGrades(int limitInPercent, int... results){
+        int cutoff = (int) (limitInPercent * maxPoints * 0.01);
+        int achieve = 0;
+        for (int result : results) {
+            if (result > cutoff){
+                achieve++;
+            }
+        }
+        return achieve;
+    }
+
+    public boolean hasAnyFailed (int limitInPercent, int... results){
+        int cutoff = (int) (limitInPercent * maxPoints * 0.01);
+        for (int result : results) {
+            if (result < cutoff){
+                return true;}
+            }
+        return false;
+        }
+    }
+}
