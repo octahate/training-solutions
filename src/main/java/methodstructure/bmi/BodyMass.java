c9a1c1868b87357cea2ca56b155d939c6d680469
octahate <geli961212@hotmail.com>
2020-12-09 19:33:18 +0100

    bmi

---
+++ src/main/java/methodstructure/bmi/BodyMass.java
@@ -0,0 +1,32 @@
+package methodstructure.bmi;
+
+public class BodyMass {
+    private double weight;
+    private double height;
+
+    public BodyMass(double weight, double height) {
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double bodyMassIndex() {
+        return weight / Math.pow(height, 2);
+    }
+
+    public BmiCategory body() {
+        double value = this.bodyMassIndex();
+        if (value < 18.5)
+            return BmiCategory.UNDERWEIGHT;
+        if (value > 25)
+            return BmiCategory.OVERWEIGHT;
+        return BmiCategory.NORMAL;
+    }
+}
