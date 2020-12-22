5709ca1d4f1e70095b1f38fad0336938b799c6d9
octahate <geli961212@hotmail.com>
2020-12-22 15:24:03 +0100

    SimpleTime done

---
+++ src/main/java/constructoroverloading/SimpleTime.java
@@ -0,0 +1,37 @@
+package constructoroverloading;
+
+import defaultconstructor.SimpleDate;
+
+public class SimpleTime {
+    private int hours;
+    private int minutes;
+
+    public SimpleTime(int hours, int minutes) {
+        this.hours = hours;
+        this.minutes = minutes;
+    }
+
+    public SimpleTime(int hours) {
+        this(hours, 0); // itt igazából a nagy konstruktorát hívom meg
+    }
+
+    public SimpleTime(SimpleTime time) {
+        this(time.hours, time.minutes);
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public int difference(SimpleTime time) {
+        return Math.abs((hours * 60 + minutes) - ((time.hours * 60 + minutes)));
+    }
+
+    public String toString() {
+        return hours + ":" + minutes;
+    }
+}
