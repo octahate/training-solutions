a692e152382be3399438c8bad0ab05c0c95d8f56
octahate <geli961212@hotmail.com>
2020-12-22 22:15:52 +0100

    employee done

---
+++ src/main/java/introinheritance/employee/Person.java
@@ -0,0 +1,15 @@
+package introinheritance.employee;
+
+public class Person {
+    private String name;
+    private String address;
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public void migrate(String newAddress) {
+        address = newAddress;
+    }
+}
