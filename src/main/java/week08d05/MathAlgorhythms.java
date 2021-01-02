4c58c794ddcea48e99280d487bf4d077a402a9e3
octahate <geli961212@hotmail.com>
2021-01-02 14:31:08 +0100

    MathAlgorhythms uh, almost done

---
+++ src/main/java/week08d05/MathAlgorhythms.java
@@ -0,0 +1,47 @@
+package week08d05;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MathAlgorhythms {
+
+    public static int greatestCommonDivisor(int first, int second) {
+        List<Integer> firtdivisors = new ArrayList<>();
+        List<Integer> seconddivisors = new ArrayList<>();
+        for (int i = first; i != 0; i--) {
+            if (first % i == 0) {
+                firtdivisors.add(i);
+            }
+        }
+        for (int j = second; j != 0; j--) {
+            if (first % j == 0) {
+                seconddivisors.add(j);
+            }
+        }
+       int solution = 1;
+       if (firtdivisors.size() > seconddivisors.size()){
+           for (int i = 0; i < firtdivisors.size(); i++) {
+               for (int j = 0; j < seconddivisors.size(); j++) {
+                   if (firtdivisors.get(i).equals(seconddivisors.get(j))){
+                       solution = firtdivisors.get(j);
+                   }
+               }
+           }
+       }
+       else {
+            for (int k = 0; k < seconddivisors.size(); k++) {
+                for (int l = 0; l < firtdivisors.size(); l++) {
+                    if (firtdivisors.get(k).equals(seconddivisors.get(l))){
+                        solution = firtdivisors.get(l);
+                    }
+                }
+            }
+        }
+       return solution;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(greatestCommonDivisor(8,4));
+    }
+}
