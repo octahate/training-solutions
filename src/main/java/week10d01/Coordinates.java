40b01d9d494ada5121588850e8958c6044e5a45e
octahate <geli961212@hotmail.com>
2021-01-04 20:16:01 +0100

    week10d01 done

---
+++ src/main/java/week10d01/Coordinates.java
@@ -0,0 +1,25 @@
+package week10d01;
+
+public class Coordinates {
+    float xCoordinate;
+    float yCoordinate;
+    float zCoordinate;
+
+    public Coordinates(float xCoordinate, float yCoordinate, float zCoordinate) {
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+        this.zCoordinate = zCoordinate;
+    }
+
+    public float getxCoordinate() {
+        return xCoordinate;
+    }
+
+    public float getyCoordinate() {
+        return yCoordinate;
+    }
+
+    public float getzCoordinate() {
+        return zCoordinate;
+    }
+}
---
+++ src/main/java/week10d01/Hiking.java
@@ -0,0 +1,41 @@
+package week10d01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hiking {
+
+    List<Coordinates> coordinates;
+    List<Float> heights;
+
+    public Hiking(List<Coordinates> allCoordinates) {
+        this.coordinates = allCoordinates;
+        for (Coordinates actualPlace : coordinates){
+            heights.add(actualPlace.zCoordinate);
+        }
+    }
+
+    public float getPlusElevation(){
+        if (heights.size() < 2 ){
+            throw new IllegalArgumentException("The coordinates list can't be empty or shorter than 2!");
+        }
+        float sum = 0;
+        for (int i = 0; i < heights.size()-1; i++) {
+            if (heights.get(i) < heights.get(i+1)){
+                sum += ((heights.get(i+1)) - heights.get(i));
+            }
+        } return sum;
+    }
+
+    public float getPlusElevation(List<Float> heights){
+        if (heights.size() < 2 ){
+            throw new IllegalArgumentException("The coordinates list can't be empty or shorter than 2!");
+        }
+        float sum = 0;
+        for (int i = 0; i < heights.size()-1; i++) {
+            if (heights.get(i) < heights.get(i+1)){
+                sum += ((heights.get(i+1)) - heights.get(i));
+            }
+        } return sum;
+    }
+}
