760bc3123b560081e62e8bd86ce1c83b64e69f62
octahate <geli961212@hotmail.com>
2021-01-05 21:10:55 +0100

    Travels done

--- src/main/java/week10d02/MaxTravel.java
+++ src/main/java/week10d02/MaxTravel.java
@@ -3,12 +3,13 @@ package week10d02;
 import java.util.List;
 
 public class MaxTravel{
+
     public static final int MAX_AMOUNT_OF_STOPS = 30;
 
     public static int getMaxIndex(List<Integer> stops){
         int[] possibleStops = new int[30];
         for (int i = 0; i < stops.size(); i++) {
-            if (stops.get(i) > MAX_AMOUNT_OF_STOPS || stops.size() < 1 ){
+            if (stops.get(i) > MAX_AMOUNT_OF_STOPS || stops.isEmpty() ){
                 throw new IllegalArgumentException("Stop list is invalid!");
             }
             possibleStops[stops.get(i)]++;
--- src/test/java/week10d02/MaxTravelTest.java
+++ src/test/java/week10d02/MaxTravelTest.java
@@ -5,12 +5,19 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MaxTravelTest {
     @Test
-    public void MaxTravelWorks(){
-        assertEquals(3,MaxTravel.getMaxIndex(Arrays.asList(2,2,1,3,3,3)));
-        assertEquals(6,MaxTravel.getMaxIndex(Arrays.asList(2,2,1,3,3,3,6,6,6,6,6)));
-        assertEquals(6,MaxTravel.getMaxIndex(Arrays.asList(2,2,1,3,3,3,6,6,6,6,6,4,4)));
+    public void MaxTravelWorks() {
+        assertEquals(3, MaxTravel.getMaxIndex(Arrays.asList(2, 2, 1, 3, 3, 3)));
+        assertEquals(6, MaxTravel.getMaxIndex(Arrays.asList(2, 2, 1, 3, 3, 3, 6, 6, 6, 6, 6)));
+        assertEquals(6, MaxTravel.getMaxIndex(Arrays.asList(2, 2, 1, 3, 3, 3, 6, 6, 6, 6, 6, 4, 4)));
+    }
+
+    @Test
+    public void testIllegalList() {
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> MaxTravel.getMaxIndex(Arrays.asList(1, 1, 1, 1, 31)));
+        assertEquals("Stop list is invalid!", ex.getMessage());
     }
 }
