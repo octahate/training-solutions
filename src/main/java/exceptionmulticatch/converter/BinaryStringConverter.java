ec8ba0dab90cfd512ed60907af75f3d1c5e2b034
octahate <geli961212@hotmail.com>
2021-01-13 20:06:45 +0100

    BinaryStringConverter done

--- src/main/java/exceptionmulticatch/converter/BinaryStringConverter.java
+++ src/main/java/exceptionmulticatch/converter/BinaryStringConverter.java
@@ -1,4 +1,38 @@
 package exceptionmulticatch.converter;
 
 public class BinaryStringConverter {
+
+    public boolean[] binaryStringToBooleanArray(String input) {
+        boolean[] result = new boolean[input.length()];
+        for (int i = 0; i < input.length(); i++) {
+            switch (input.charAt(i)) {
+                case '1':
+                    result[i] = true;
+                    break;
+                case '0':
+                    result[i] = false;
+                    break;
+                default:
+                    throw new IllegalArgumentException("Input String invalid!");
+            }
+        }
+        return result;
+    }
+
+    public String booleanArrayToBinaryString(boolean[] input) {
+        StringBuilder result = new StringBuilder();
+        if (input.length == 0) {
+            throw new IllegalArgumentException("Input Array invalid!");
+        }
+        for (int i = 0; i < input.length; i++) {
+            if (input[i]) {
+                result.append('1');
+            } else if (!(input[i])) {
+                result.append('0');
+            }
+        }
+        return result.toString();
+    }
+
 }
+
