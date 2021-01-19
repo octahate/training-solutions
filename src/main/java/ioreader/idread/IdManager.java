437eb72623a67d9deefef549c0f5da8182da83af
octahate <geli961212@hotmail.com>
2021-01-19 15:21:57 +0100

    idmanager done

--- src/main/java/ioreader/idread/IdManager.java
+++ src/main/java/ioreader/idread/IdManager.java
@@ -8,17 +8,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class IdManager {
-    List<String> Ids = new ArrayList<>();
 
-    public IdManager() {
-    }
+    private List<String> Ids = new ArrayList<>();
+
 
     public List<String> getIds() {
-        return Ids;
+        return new ArrayList<>(Ids);
     }
 
     public void readIdsFromFile(String filename){
-        Path file = Path.of(filename);
+        Path file = Path.of("src/main/resources/idread/" + filename);
         try (BufferedReader reader = Files.newBufferedReader(file)) {
             String nextLine;
             while ((nextLine = reader.readLine()) != null){
