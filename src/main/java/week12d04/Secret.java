7ddb51662eb2672c1b7b816ec2568708a23a52eb
octahate <geli961212@hotmail.com>
2021-01-21 16:09:11 +0100

    Secret done

--- src/main/java/week12d04/Secret.java
+++ src/main/java/week12d04/Secret.java
@@ -9,6 +9,20 @@ import java.util.Arrays;
 
 public class Secret {
 
+    public void printSecret(Path path) {
+
+        try {
+            byte[] bytes = Files.readAllBytes(path);
+            for (byte b : bytes) {
+                char c = (char) (b + 10);
+                System.out.print(c);
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException("Can not read file!");
+        }
+
+    }
+
 
     public static void main(String[] args) {
         Path file = Path.of("src/main/java/week12d04/secret.dat");
@@ -24,5 +38,8 @@ public class Secret {
                 IOException ioe) {
             throw new IllegalStateException("Can not read file", ioe);
         }
+
+        Secret secret = new Secret();
+        secret.printSecret(file);
     }
 }
